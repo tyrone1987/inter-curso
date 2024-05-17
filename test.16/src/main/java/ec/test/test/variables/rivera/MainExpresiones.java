960ceaa122f2b7885b3d75ba9b1df49564366e3c
luiswiwi <luiswiwi1@hotmail.com>
2024-05-17 18:41:32 -0500

    modificado Luis mRivera

---
+++ test.16/src/main/java/ec/test/test/variables/rivera/MainExpresiones.java
@@ -0,0 +1,20 @@
+package ec.test.test.variables.rivera;
+
+public class MainExpresiones {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Expresiones expresiones = new Expresiones();
+		int suma = expresiones.sumar(5, 8);
+		expresiones.saludar();
+		System.out.println(suma);
+		int a= 0;
+		int b= 5;
+		boolean result = a > b;
+		suma = a + b;
+		
+
+	}
+
+}
