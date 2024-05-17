83c98db4119804bd2fd3f7a9524a85d59d733205
Tyrone <tyrone_lv@hotmail.com>
2024-05-17 18:39:25 -0500

    expresiones

---
+++ test.16/src/main/java/ec/test/test/variables/lopez/Expresiones.java
@@ -0,0 +1,12 @@
+package ec.test.test.variables.lopez;
+
+public class Expresiones {
+	public int sumar(int a, int b) {
+		int res = a + b;
+		return res;
+	}
+	
+	public void saludar() {
+		System.out.println("Hola ");
+	}
+}
---
+++ test.16/src/main/java/ec/test/test/variables/lopez/MainExpresiones.java
@@ -0,0 +1,35 @@
+package ec.test.test.variables.lopez;
+
+public class MainExpresiones {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Expresiones expresiones = new Expresiones();
+		/**Expresion
+		 * las expresiones son combinaciones de operadores
+		 * o llamadas a metodos
+		 * 
+		 * */
+		 
+		int suma = expresiones.sumar(5, 8);
+		
+		expresiones.saludar();
+		System.out.println(suma);
+		
+		//suma = a +b ;
+		
+		/*Sentencias 
+		 * 
+		 * las sentencias sirven para 
+		 * 
+		 * */
+		
+		int a1 = 0;
+		int b1 = 5;
+		
+		int a = 0; // Sentencia
+		int b = 5; // Sentencia
+		boolean result = a > b;
+	}
+
+}
