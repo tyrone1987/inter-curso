6c9b04d7de936e0933035fc06b9c698159239a98
tyrone <tyrone_lv@hotmail.com>
2024-05-28 18:49:29 -0500

    Update Cliente.java

--- prueba-maven/src/main/java/ec/com/banco/internacional/models/Cliente.java
+++ prueba-maven/src/main/java/ec/com/banco/internacional/models/Cliente.java
@@ -1,26 +1,16 @@
 package ec.com.banco.internacional.models;
 
 import java.security.Security;
-import java.time.LocalDateTime;
 import java.util.Date;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 public class Cliente extends Persona {
 
-
 	private String nombre;
-
 
 	private int edad;
 	
-
 	private String genero;
 	
-
 	private Date fehcaNacimiento;
 
 	private Agencia agencia;
