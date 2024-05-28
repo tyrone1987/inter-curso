7ce9e788fd7d0a7b668371a74c05ba42e4080ede
tyrone <tyrone_lv@hotmail.com>
2024-05-28 17:39:19 -0500

    entidaes

--- prueba-maven/src/main/java/ec/com/banco/internacional/base/datos/modelos/Usuario.java
+++ prueba-maven/src/main/java/ec/com/banco/internacional/base/datos/modelos/Usuario.java
@@ -1,9 +1,25 @@
 package ec.com.banco.internacional.base.datos.modelos;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Table(name = "users")
+@Entity
 public class Usuario {
-	private Long id;
 	
-	private String name;
+	@Id
+	@Column(name = "id_user")
+	//@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name = "name_user", length = 100)
+	private String nombre;
 	
-	private String cedula;
+	private String apellido;
 }
