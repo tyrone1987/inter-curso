4c36d8338b26f3a9b56b14b026740065918ca66e
tyrone <tyrone_lv@hotmail.com>
2024-05-30 16:38:20 -0500

    agregando proyecto web

--- bases-datos/src/main/java/ec/test/inter/modelos/derby/Usuario.java
+++ bases-datos/src/main/java/ec/test/inter/modelos/derby/Usuario.java
@@ -1,8 +1,18 @@
 package ec.test.inter.modelos.derby;
 
+import javax.persistence.ColumnResult;
+import javax.persistence.ConstructorResult;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.SqlResultSetMapping;
 
+import ec.test.inter.vistas.derby.PermisosUsuarioVista;
+
+@SqlResultSetMapping(name = "permisos_usuarios", classes = {
+		@ConstructorResult(targetClass = PermisosUsuarioVista.class, columns = {
+				@ColumnResult(name = "nombre_permiso"), 
+				@ColumnResult(name = "nombre"), 
+				@ColumnResult(name = "id_user"),}) })
 @Entity
 public class Usuario {
 
---
+++ mi-proyecto-web/src/main/java/ec/com/internacional/AplicacionConfiguracion.java
@@ -0,0 +1,9 @@
+package ec.com.internacional;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("apis/banco")
+public class AplicacionConfiguracion extends Application {
+
+}
